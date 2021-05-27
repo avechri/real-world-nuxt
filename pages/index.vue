17818afc60d06ab3fb0bc8db107db3895ae9ec7e
kristina <christychri0@gmail.com>
2021-05-27 18:15:18 +0700

    added error handling when API falls

--- pages/index.vue
+++ pages/index.vue
@@ -16,12 +16,21 @@ export default {
   components: {
     EventCard,
   },
-  asyncData({ $axios }) {
-    return $axios.get('http://localhost:3000/events').then((response) => {
-      return {
-        events: response.data, // merges with component data
-      }
-    })
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return {
+          events: response.data, // merges with component data
+        }
+      })
+      .catch((e) => {
+        error({
+          statusCode: 503,
+          message:
+            'Unable to fetch events at this time. Please, try again later',
+        })
+      })
   },
   // how vue-meta works
   head() {
