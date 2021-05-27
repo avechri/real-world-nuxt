e72397d61e5bcb1ec147d8a963113b83e184decd
kristina <christychri0@gmail.com>
2021-05-27 18:04:02 +0700

    refactored code using ES6

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,12 @@
 
 <script>
 export default {
-  asyncData(context) {
-    return context.$axios
-      .get('http://localhost:3000/events')
-      .then((response) => {
-        return {
-          events: response.data, // merges with component data
-        }
-      })
+  asyncData($axios) {
+    return $axios.get('http://localhost:3000/events').then((response) => {
+      return {
+        events: response.data, // merges with component data
+      }
+    })
   },
   // how vue-meta works
   head() {
