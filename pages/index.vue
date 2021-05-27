455feb01429fa005e8bd0c37a237957a45e17c34
kristina <christychri0@gmail.com>
2021-05-27 18:49:50 +0700

    changed Promise nesting to async/await

--- pages/index.vue
+++ pages/index.vue
@@ -16,21 +16,18 @@ export default {
   components: {
     EventCard,
   },
-  asyncData({ $axios, error }) {
-    return $axios
-      .get('http://localhost:3000/events')
-      .then((response) => {
-        return {
-          events: response.data, // merges with component data
-        }
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await $axios.get('http://localhost:3000/events')
+      return {
+        events: data, // merges with component data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time. Please, try again later',
       })
-      .catch((e) => {
-        error({
-          statusCode: 503,
-          message:
-            'Unable to fetch events at this time. Please, try again later',
-        })
-      })
+    }
   },
   // how vue-meta works
   head() {
