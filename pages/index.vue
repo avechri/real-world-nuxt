3fbd323af828511854c3981b6817c949f7d1843b
kristina <christychri0@gmail.com>
2021-05-27 18:11:47 +0700

    added EventCard.vue component to the proj

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '~/components/EventCard'
 export default {
+  components: {
+    EventCard,
+  },
   asyncData({ $axios }) {
     return $axios.get('http://localhost:3000/events').then((response) => {
       return {
